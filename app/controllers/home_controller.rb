7221e26b709f63b497e380e5a3689e2f7947bee6
Mike James <mikejames386@gmail.com>
2017-04-25 11:09:12 -0500

    implement ajax to populate post partial in body

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,11 @@ class HomeController < ApplicationController
   def index
     @posts = Post.paginate(:page => params[:page], :per_page => 20)
   end
+
+  def fetch_post
+    @post = Post.find(params[:post_id])
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "home#index", as: 'root'
+  get '/fetch_post' => 'home#fetch_post', as: 'fetch_post'
 end
