1c7b532ae0feeb173ba2a2e2e6b4c61f9d9c7da8
Mike James <mikejames386@gmail.com>
2017-04-24 23:40:17 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+100.times do
+  Post.create(
+    body: Faker::Lorem.paragraph,
+    post_time: Faker::Time.between(DateTime.now - 3, DateTime.now),
+    image_url: 'https://placeholdit.imgix.net/~text?txtsize=6&txt=50%C3%9750&w=50&h=50'
+  )
+end
